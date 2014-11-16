dcafb76ca398c1e7359534aeba88927cb3cf9ee3
DanielAguado <danielaguadoaraujo@gmail.com>
2014-11-17 00:09:41 +0100

    A?adida la opcion de poner bandera a la celda e impedir que cambie de estado

--- cell.cpp
+++ cell.cpp
@@ -3,13 +3,30 @@
 Cell::Cell(){
   _type = Cell::Type::Empty;
   _state = Cell::State::NotVisible;
+  _flag = Flag::NotFlagged;
 }
 
 void
 Cell::initialize(Cell::Type type, Cell::State state) {
   _type = type;
   _state = state;
+  _flag = Flag::NotFlagged;
 };
+
+void 
+Cell::put_flag() {
+	_flag = Flag::Flagged;
+}
+
+void
+Cell::remove_flag() {
+	_flag = Flag::Flagged;
+}
+
+bool
+Cell::is_flagged() {
+	return (_flag ==Flag::Flagged);
+}
 
 bool
 Cell::is_bomb() {
@@ -24,6 +41,12 @@ Cell::get_type() {
 Cell::State
 Cell::is_visible() {
   return _state;
+}
+
+void
+Cell::execute() {
+	if(_flag != Flag::Flagged && _state != State::Visible)
+		_state = State::Visible;
 }
 
 void
--- include/cell.h
+++ include/cell.h
@@ -5,8 +5,9 @@
 
 class Cell {
  public:
-  enum Type{Bomb=-1, Empty=0, Near=1};
-  enum State{Visible, NotVisible};
+  enum Type {Bomb=-1, Empty=0, Near=1};
+  enum State {Visible, NotVisible};
+  enum Flag {Flagged, NotFlagged};
 
   Cell();
   void initialize(Type type, State state);
@@ -14,11 +15,16 @@ class Cell {
   State is_visible();
   bool is_bomb();
   void chage_type_near_to_bomb();
+  void put_flag();
+  void remove_flag();
+  void execute();
+  bool is_flagged();
 
  private:
   Type _type;
   std::vector<Cell> _neightbors;
   State _state;
+  Flag _flag;
 };
 
 #endif
